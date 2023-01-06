e96dff9bca701e32f276f502839e745e6192c36e
Alexander-Souza <bsalex12010@gmail.com>
2023-01-05 21:03:08 -0300

    Modificações necessárias.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-/* pegando caracter escolhido pelo cliente */
+/* pegando caracter escolhido pelo cliente e guardando na variável caracter1
+var caracter1 = document.getElementsByClassName("Escolhas") 
+console.log(
+    caracter1
+)
 
+/* pegando caracter não escolhido pelo cliente e guardando na variável caracter2
+if(caracter1 = value="x")
+var caracter2 = "O"
+else 
+var caracter2 = "X"
+ console.log(
+caracter1
+ )
+/*mapeando a velha*/
+/*olhar console log*/
