655c159b2b6840f682839349f11d9336b7112e0d
Alexander Batista de Souza <Alexander.souza@utramigbh.sede>
2023-01-06 14:42:48 -0300

    função escolher caracter.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-/* pegando caracter escolhido pelo cliente e guardando na variável caracter1
-var caracter1 = document.getElementsByClassName("Escolhas") 
-console.log(
-    caracter1
-)
+/* pegando caracter escolhido pelo cliente e guardando na variável caracter1 */
 
-/* pegando caracter não escolhido pelo cliente e guardando na variável caracter2
-if(caracter1 = value="x")
-var caracter2 = "O"
-else 
-var caracter2 = "X"
- console.log(
-caracter1
- )
-/*mapeando a velha*/
-/*olhar console log*/
+
+function clickTeclado(letra){
+    alert(letra);
+}
+
+
