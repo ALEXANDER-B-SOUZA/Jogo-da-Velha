f3edc9050ad2dd91696979a4907d161175b3a7c0
Alexander Batista de Souza <119431061+ALEXANDER-B-SOUZA@users.noreply.github.com>
2023-02-17 21:06:55 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,6 +36,7 @@ while (contador <= 9){
    } else{
     playO = escolha;
 }
+     
 /*verificando o vencedor*/
 if(playX == 1,2,3 || 4,5,6 || 7,8,9 || 1,4,7 || 2,5,8 || 3,6,9 || 1,5,9 || 3,5,7){
     alert("O jogador X é o vencedor!");
